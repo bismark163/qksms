592866167044051eb20f85fd6a3c3a4a558cceff
Кирилл Максименков <kirill.maksimenkov@e-legion.com>
2019-09-25 21:53:02 +0300

    Add new test Add new dependencies

--- presentation/src/androidTest/java/com/moez/QKSMS/feature/main/MainViewModelTest.kt
+++ presentation/src/androidTest/java/com/moez/QKSMS/feature/main/MainViewModelTest.kt
@@ -18,16 +18,75 @@
  */
 package com.moez.QKSMS.feature.main
 
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
+import com.moez.QKSMS.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.lang.Thread.sleep
+
 
 @RunWith(AndroidJUnit4::class)
 class MainViewModelTest {
 
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
     @Test
-    fun insertedSentMessageEqualsSyncResult() {
-        assert(1 == 1)
+    fun openDIalogWithContact() {
+        onView(withId(R.id.compose)).perform(click())
+        onView(withId(R.id.chips)).perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0, typeText("qwdqwdqd")))
+        onView(withId(R.id.contacts))
+                .perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.message)).perform(typeText("test 1"))
+        onView(withId(R.id.send)).perform(click())
+
+        sleep(10000)
+
+
+
     }
 
+
+
+
+
+
+    private fun childAtPosition(
+            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+
+     fun focusAndEnterText(text: String)  {onView(
+            Matchers.allOf(childAtPosition(
+                    Matchers.allOf(withId(R.id.chips),
+                            childAtPosition(
+                                    withClassName(Matchers.`is`("android.widget.LinearLayout")),
+                                    2)),
+                    0),
+                    isDisplayed())).perform(click(), typeText(text))}
 }
