a1ec235771c4e50357a01e246626642431ccfb23
Кирилл Максименков <kirill.maksimenkov@e-legion.com>
2019-09-26 21:32:03 +0300

    Add new one test

--- presentation/src/androidTest/java/com/moez/QKSMS/feature/main/MainViewModelTest.kt
+++ presentation/src/androidTest/java/com/moez/QKSMS/feature/main/MainViewModelTest.kt
@@ -20,14 +20,22 @@ package com.moez.QKSMS.feature.main
 
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewStub
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Root
 import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItem
 import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
+
 import com.moez.QKSMS.R
+import org.hamcrest.CoreMatchers.allOf
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers
@@ -44,17 +52,23 @@ class MainViewModelTest {
     @get:Rule
     val activityRule = ActivityTestRule(MainActivity::class.java)
 
+    val test_text: String = "teeest"
+
     @Test
     fun openDIalogWithContact() {
         onView(withId(R.id.compose)).perform(click())
-        onView(withId(R.id.chips)).perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0, typeText("qwdqwdqd")))
+        onView(withId(R.id.chips)).perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0, typeText("First Contact")))
         onView(withId(R.id.contacts))
                 .perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
-        onView(withId(R.id.message)).perform(typeText("test 1"))
+        onView(withId(R.id.message)).perform(typeText(test_text))
         onView(withId(R.id.send)).perform(click())
+        onView(withText(test_text)).check(matches(withText(test_text)))
+    }
 
-        sleep(10000)
-
+    @Test
+    fun sendConversationToArchieve() {
+        onView(withId(R.id.title)).perform(swipeLeft())
+        onView(withText("Archived conversation")).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
 
 
     }
@@ -89,4 +103,6 @@ class MainViewModelTest {
                                     2)),
                     0),
                     isDisplayed())).perform(click(), typeText(text))}
+
+
 }
